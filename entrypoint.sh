4b2ec54fc14bc52ae3b93fff5fa5d03ecefac0c6
David Wesst <david@cocobokostudios.com>
2020-10-13 16:31:50 -0500

    changed of flags in entrypoint export command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -l
 
 # copy templates to github directory
-mkdir -v -p ${HOME}/.local/share/godot/templates/${GODOT_VERSION}.stable.mono/
+mkdir -v -p ${HOME}/.local/share/godot/templates/${GODOT_VERSION}.stable.mono/ 
 cp -R /root/.local/share/godot/templates/${GODOT_VERSION}.stable.mono/* ${HOME}/.local/share/godot/templates/${GODOT_VERSION}.stable.mono/
 
 # make export directory
 mkdir -v -p $(dirname $3)
 
 # export project
-godot --path $1 --verbose --quit --export $2 $3
+godot --path $1 --export $2 $3 --verbose --quit
